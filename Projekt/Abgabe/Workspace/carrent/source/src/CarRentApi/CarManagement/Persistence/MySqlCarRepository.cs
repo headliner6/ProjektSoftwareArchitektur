9e7bd47efefdf3c870501b56c49d7bfc8cc04688
M. Waldburger <waldburger91@hotmail.com>
2019-02-14 18:32:22 +0100

    Add SQL DDL Scrip and MySqlCarRepo

--- Projekt/Abgabe/Workspace/carrent/source/src/CarRentApi/CarManagement/Persistence/MySqlCarRepository.cs
+++ Projekt/Abgabe/Workspace/carrent/source/src/CarRentApi/CarManagement/Persistence/MySqlCarRepository.cs
@@ -14,10 +14,25 @@ namespace CarRent.API.CarManagement.Persistence
 
         public IReadOnlyList<Car> GetAll()
         {
+            var carsAsStringsList = new List<string>();
+            var cars = new List<Car>();
             _mySqlConnection.Open();
-            using (_mySqlConnection.BeginTransaction())
+            using (var cmd = _mySqlConnection.CreateCommand())
             {
-                return null;
+                cmd.CommandText = "SELECT * FROM CarRentDB.Cars";
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (var i = 1; i < 7; i++)
+                        {
+                            var data = reader.GetValue(i);
+                            carsAsStringsList.Add((data == null ? "" : data.ToString()));
+                            
+                        }
+                    }
+                }
+                return null /*cars*/;
             }
         }
     }
--- Projekt/Abgabe/Workspace/carrent/source/src/CarRentApi/Startup.cs
+++ Projekt/Abgabe/Workspace/carrent/source/src/CarRentApi/Startup.cs
@@ -24,6 +24,7 @@ namespace CarRent.API
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddTransient<ICarService, CarService>();
             services.AddTransient<ICarRepository, CarRepository>();
+            services.AddTransient<ICarRepository, MySqlCarRepository>(sp => new MySqlCarRepository("Server = localhost; Database = CarRentDB; Uid = root; Pwd = password"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
